e45cd01ae1eccd6a99da3168b93393e1558595f1
Karaytumut20 <karaytumut20@gmail.com>
2025-11-07 16:20:29 +0300

    asdada

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        mugessss
+      </Text>
+    </View>
+  );
+};
+
+// Yazıyı ortalamak için basit stiller
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  }
+});
+
+export default App;
